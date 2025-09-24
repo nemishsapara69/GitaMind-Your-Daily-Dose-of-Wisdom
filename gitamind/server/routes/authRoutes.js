a6eb232691da2b7b70e57be9890b364c61017d9e
nemishsapara69 <164607451+nemishsapara69@users.noreply.github.com>
2025-09-24 22:37:39 +0530

    here i make by mistake second server folder so i remove this

--- gitamind/server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController'); // Import the controller
-
-// Public Routes
-router.post('/register', authController.registerUser); // Route for user signup
-router.post('/login', authController.loginUser);     // Route for user login
-
-module.exports = router;
