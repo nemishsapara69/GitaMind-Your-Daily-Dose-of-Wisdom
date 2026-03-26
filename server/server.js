f65f02ed11e63f271dddd024f10490f7576befaa
nemishsapara69 <164607451+nemishsapara69@users.noreply.github.com>
2026-03-26 12:00:56 +0530

    fix: use Express 5 compatible CORS preflight route

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 
 // --- Database Connection ---
